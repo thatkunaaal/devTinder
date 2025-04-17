68b401b2fa5eb97efd729055d9525a9b23f1b81f
thatkunaaal <thatkunaal@gmail.com>
2025-04-17 08:39:20 +0530

    :bug syncing jwt token for 7 days

--- src/models/user.js
+++ src/models/user.js
@@ -105,7 +105,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.methods.getJWT = async function(){
   const user = this;
-  const token = await jwt.sign({"_id" : user._id},process.env.JWT_SECRET,{expiresIn: "1d"});
+  const token = await jwt.sign({"_id" : user._id},process.env.JWT_SECRET,{expiresIn: "7d"});
   return token;
 }
 
